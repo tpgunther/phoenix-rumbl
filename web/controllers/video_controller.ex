cea359dd5434d9dbf84b9d6ac42e61ac2b9ffe2d
Tomas Gunther <tomasgunther@gmail.com>
2017-12-11 21:36:05 -0300

    user has many videos and are associated on create

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -3,18 +3,25 @@ defmodule Rumbl.VideoController do
 
   alias Rumbl.Video
 
-  def index(conn, _params) do
+  def index(conn, _params, user) do
     videos = Repo.all(Video)
     render(conn, "index.html", videos: videos)
   end
 
-  def new(conn, _params) do
-    changeset = Video.changeset(%Video{})
+  def new(conn, _params, user) do
+    changeset =
+      user
+      |> build_assoc(:videos)
+      |> Video.changeset()
+
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"video" => video_params}) do
-    changeset = Video.changeset(%Video{}, video_params)
+  def create(conn, %{"video" => video_params}, user) do
+    changeset =
+      user
+      |> build_assoc(:videos)
+      |> Video.changeset(video_params)
 
     case Repo.insert(changeset) do
       {:ok, _video} ->
@@ -61,5 +68,9 @@ defmodule Rumbl.VideoController do
     conn
     |> put_flash(:info, "Video deleted successfully.")
     |> redirect(to: video_path(conn, :index))
+  end
+
+  def action(conn, _) do
+    apply(__MODULE__, action_name(conn), [conn, conn.params, conn.assigns.current_user])
   end
 end
